19fdc16b884d5a7cb4202a476f8c2e287a4f78d3
Duy Nguyen <tapvanvn@gmail.com>
2021-12-03 23:46:43 +0700

    update

--- eth/client.go
+++ eth/client.go
@@ -4,20 +4,19 @@ import (
 	"crypto/ecdsa"
 
 	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/tapvanvn/goauth/common"
+	"github.com/tapvanvn/goauth"
 )
 
 type Client struct {
-	address string
-
+	address   string
 	publicKey *ecdsa.PublicKey
 }
 
-func (client *Client) VerifySignature(doc common.Document, signature []byte) (bool, error) {
+func (client *Client) VerifySignature(doc goauth.Document, signature []byte) (bool, error) {
 
 	if signature[64] != 27 && signature[64] != 28 {
 
-		return false, common.ErrInvalidSignature
+		return false, goauth.ErrInvalidSignature
 	}
 	signature[64] -= 27
 
@@ -32,4 +31,18 @@ func (client *Client) VerifySignature(doc common.Document, signature []byte) (bo
 	recoveredAddr := crypto.PubkeyToAddress(*pubKey).Hex()
 
 	return client.address == recoveredAddr, nil
+}
+
+func (client *Client) GetClientType() goauth.ClientType {
+
+	return goauth.ClientTypeEthereum
+}
+
+func (client *Client) BeginSession(clientID goauth.AccountID, adapter goauth.IAdapter) (goauth.ISession, error) {
+	sessionID := adapter.NewSessionID()
+
+	return NewSession(sessionID), nil
+}
+func (client *Client) Verify(session goauth.ISession, adapter goauth.IAdapter) (bool, error) {
+
 }
